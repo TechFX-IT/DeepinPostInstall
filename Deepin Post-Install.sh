6f89d7c433059c0ac0fc1f12b60e6ee3ac07eb72
TechFX <modderwindowsandroid@gmail.com>
2017-08-08 12:50:52 +0200

    Update Deepin Post-Install.sh

--- Deepin Post-Install.sh
+++ Deepin Post-Install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #IT
-#Script compilato da TechFX (https://github.com/TechFX-IT) su base di quello creato da Massimo A. Carlofano (https://github.com/Maxximo88), appositamente per facilitare la post-installazione dell'OS Deepin Linux. Sarà possibile utilizzare lo script con qualsiasi distri usi APT, ma potrebbero esserci problemi di dipendenze.
+#Script compilato da TechFX (https://github.com/TechFX-IT) su base di quello creato da Massimo A. Carofano (https://github.com/Maxximo88), appositamente per facilitare la post-installazione dell'OS Deepin Linux. Sarà possibile utilizzare lo script con qualsiasi distri usi APT, ma potrebbero esserci problemi di dipendenze.
 #Copyright (c) 2017 TechFX.
 #Questo programma è un software gratuito distribuito sotto licenza GNU, versione 3.
 #Il software viene distribuito senza alcuna garanzia, nemmeno per le organizzazioni commerciali.
 
 #EN
-#Script compiled by TechFX (https://github.com/TechFX-IT) using the one compiled by Massimo A. Carlofano (https://github.com/Maxximo88) as base, just to simplify the Deepin post-installation process. Will be possible to use this script with every Linux Distribuion that uses APT, but may be problems with dependencies.
+#Script compiled by TechFX (https://github.com/TechFX-IT) using the one compiled by Massimo A. Carofano (https://github.com/Maxximo88) as base, just to simplify the Deepin post-installation process. Will be possible to use this script with every Linux Distribuion that uses APT, but may be problems with dependencies.
 #Copyright (c) 2017 TechFX.
 #This program is a free software, published with GNU licence, version 3.
 #The software doesn't come with any kind of warranty, even not for commercial purposes.
